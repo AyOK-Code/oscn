ea28d9c61d656e68c7ba22eaf674b6013bb438cc
Holden Mitchell <htmitchell90@gmail.com>
2021-08-14 06:24:32 -0500

    Make updates

--- app/models/report_warrants.rb
+++ app/models/report_warrants.rb
@@ -1,5 +1,9 @@
 # Database view that feeds a PowerBI report
 class ReportWarrants
+  def self.refresh
+    Scenic.database.refresh_materialized_view(:report_warrants, concurrently: false, cascade: false)
+  end
+
   def readonly?
     true
   end
--- db/migrate/20210629110239_add_debit_credit_to_docket_event.rb
+++ db/migrate/20210629110239_add_debit_credit_to_docket_event.rb
@@ -3,7 +3,5 @@ class AddDebitCreditToDocketEvent < ActiveRecord::Migration[6.0]
     add_column :docket_events, :payment, :decimal, default: 0, null: false
     add_column :docket_events, :adjustment, :decimal, default: 0, null: false
     add_index :docket_events, :amount, where: "amount != 0"
-    add_index :docket_events, :payment
-    add_index :docket_events, :adjustment
   end
 end
