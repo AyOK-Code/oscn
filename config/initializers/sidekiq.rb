e981cabbdb6733983da04171c55bfd1a4def4436
Holden Mitchell <htmitchell90@gmail.com>
2021-09-10 04:52:27 -0500

    Add more sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 require 'sidekiq/throttled'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL_NO_EVICTION'] || ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL_NO_EVICTION'] || ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
 end
 
 # Create oscn throttling strategy
