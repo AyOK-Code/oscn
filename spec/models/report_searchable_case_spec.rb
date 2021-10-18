af89988e112e04196b484eba45b7a62c49671865
Holden Mitchell <htmitchell90@gmail.com>
2021-10-18 10:50:01 -0500

    Adjust workflow

---
+++ spec/models/report_searchable_case_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe PartyStat, type: :model do
+  describe 'view data' do
+    it 'has a row for each count on a case' do
+
+    end
+
+    it 'calculates active warrants' do
+
+    end
+
+    it 'calculates inactive warrants' do
+
+    end
+
+    it 'extracts the title code from the filed_statute_violation' do
+
+    end
+  end
+end
