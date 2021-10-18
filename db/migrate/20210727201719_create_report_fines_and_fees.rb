386bcf96e7d51ddfa386a3154d3cfe3d46ffd1eb
Holden Mitchell <htmitchell90@gmail.com>
2021-10-18 11:33:26 -0500

    Remove comments from migrations

--- db/migrate/20210727201719_create_report_fines_and_fees.rb
+++ db/migrate/20210727201719_create_report_fines_and_fees.rb
@@ -2,10 +2,10 @@ class CreateReportFinesAndFees < ActiveRecord::Migration[6.0]
   def change
     add_index :docket_events, :payment, where: "payment != 0"
     add_index :docket_events, :adjustment, where: "adjustment != 0"
-    # create_view :report_fines_and_fees, materialized: true
-    # add_index :report_fines_and_fees, :court_case_id
-    # add_index :report_fines_and_fees, :case_type_id
-    # add_index :report_fines_and_fees, :docket_event_types_id
-    # add_index :report_fines_and_fees, :event_on
+    create_view :report_fines_and_fees, materialized: true
+    add_index :report_fines_and_fees, :court_case_id
+    add_index :report_fines_and_fees, :case_type_id
+    add_index :report_fines_and_fees, :docket_event_types_id
+    add_index :report_fines_and_fees, :event_on
   end
 end
