f7d518bced057b0d2f6b4725feb4ff650c7263e5
Holden Mitchell <htmitchell90@gmail.com>
2021-10-18 10:54:38 -0500

    Appease the cop

--- app/models/docket_event_type.rb
+++ app/models/docket_event_type.rb
@@ -1,5 +1,6 @@
 class DocketEventType < ApplicationRecord
-  WARRANT_CODES = ['WAI$', 'BWIFAP', 'BWIFA', 'BWIFC', 'BWIAR', 'BWIAA', 'BWICA', 'BWIFAR', 'BWIFAA', 'BWIFP', 'BWIMW', 'BWIR8', 'BWIS', 'BWIS$', 'WAI', 'WAIMV', 'WAIMW', 'BWIFAR']
+  WARRANT_CODES = ['WAI$', 'BWIFAP', 'BWIFA', 'BWIFC', 'BWIAR', 'BWIAA', 'BWICA', 'BWIFAR', 'BWIFAA', 'BWIFP', 'BWIMW',
+                   'BWIR8', 'BWIS', 'BWIS$', 'WAI', 'WAIMV', 'WAIMW', 'BWIFAR']
 
   has_many :docket_events, dependent: :destroy
 
