e4715cee371462537eef4bddbc7bdd3258a4c1bc
Holden Mitchell <htmitchell90@gmail.com>
2021-10-18 10:38:43 -0500

    Add spec for party stats

--- spec/models/views/report_fines_and_fees_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ReportFinesAndFees, type: :view do
-  describe 'view data' do
-    it 'creates view from underlying data' do
-      skip
-    end
-  end
-end
