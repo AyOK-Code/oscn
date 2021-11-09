f9742a25c831ed5b4c155e03d8a6113089068103
Holden Mitchell <htmitchell90@gmail.com>
2021-11-09 15:26:09 -0600

    Fix failing specs

--- spec/factories/count_codes.rb
+++ spec/factories/count_codes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :count_code do
-    code { 'MyString' }
-    description { 'MyString' }
+    code { Faker::Name.unique.initials }
+    description { Faker::Lorem.sentence }
   end
 end
