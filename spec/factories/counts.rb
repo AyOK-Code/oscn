722c8dff3ef684c173eae008d910530dd0b68613
Holden Mitchell <htmitchell90@gmail.com>
2021-11-09 15:26:09 -0600

    Fix failing specs

--- spec/factories/counts.rb
+++ spec/factories/counts.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     offense_on { Faker::Time.between(from: DateTime.now - 100, to: DateTime.now) }
     as_filed { Faker::Lorem.sentence }
     filed_statute_violation { "#{Faker::Number.number(digits: 2)} O.S. #{Faker::Number.number(digits: 3)}" }
+    association :filed_statute_code, factory: :count_code
     plea
     verdict
   end
