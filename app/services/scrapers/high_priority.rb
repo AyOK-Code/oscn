669c947eb595c9bf76c679044cb1ef038392dabb
Holden Mitchell <htmitchell90@gmail.com>
2021-11-12 13:36:44 -0600

    Set up scraper to scrape future cases as high priority

--- app/services/scrapers/high_priority.rb
+++ app/services/scrapers/high_priority.rb
@@ -1,15 +1,16 @@
 module Scrapers
   # Updates html for cases that have been on the docket in the past week or cases without html (new cases)
   class HighPriority
-    attr_accessor :days_ago, :county
+    attr_accessor :days_ago, :county, :days_forward
 
-    def initialize(county, days_ago: 7)
+    def initialize(county, days_ago: 7, days_forward: 7)
       @days_ago = days_ago
       @county = county
+      @days_forward = days_forward
     end
 
-    def self.perform(county, days_ago: 7)
-      new(county, days_ago: days_ago).perform
+    def self.perform(county, days_ago: 7, days_forward: 7)
+      new(county, days_ago: days_ago, days_forward: days_forward).perform
     end
 
     def perform
@@ -26,8 +27,8 @@ module Scrapers
 
     def fetch_case_list
       missing_html_cases = CourtCase.for_county_name(county).without_html.pluck(:case_number)
-      recent_cases = Scrapers::RecentCases.perform(county, days_ago: days_ago)
-      (missing_html_cases + recent_cases).flatten
+      recent_cases = Scrapers::RecentCases.perform(county, days_ago: days_ago, days_forward: days_forward)
+      (missing_html_cases + recent_cases).flatten.uniq
     end
   end
 end
