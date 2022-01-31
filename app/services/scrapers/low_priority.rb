b10698fc11662d44528bf8196679f9b066fd7520
Holden Mitchell <htmitchell90@gmail.com>
2022-01-31 15:34:57 -0600

    Add env variables for low and medium priority scraping

--- app/services/scrapers/low_priority.rb
+++ app/services/scrapers/low_priority.rb
@@ -3,12 +3,12 @@ module Scrapers
   class LowPriority
     attr_accessor :cases
 
-    def initialize(days_ago: 90, limit: 7500)
+    def initialize(days_ago: 90, limit: low_count)
       @cases = CourtCase.closed.older_than(days_ago.days.ago).limit(limit)
     end
 
-    def self.perform(days_ago: 90, limit: 7500)
-      new(days_ago: days_ago, limit: limit).perform
+    def self.perform(days_ago: 90)
+      new(days_ago: days_ago).perform
     end
 
     def perform
@@ -21,6 +21,12 @@ module Scrapers
           .perform_async({ county_id: c.county_id, case_number: c.case_number, scrape_case: true })
         bar.increment!
       end
+    end
+
+    private
+
+    def low_count
+      ENV.fetch('LOW_PRIORITY', 10000).to_i
     end
   end
 end
