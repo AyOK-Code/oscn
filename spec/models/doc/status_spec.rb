454cf0837289ef986a10c2f4ffc9b7c49493634d
Holden Mitchell <htmitchell90@gmail.com>
2022-03-10 14:14:21 -0600

    Fix failing spec

--- spec/models/doc/status_spec.rb
+++ spec/models/doc/status_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Doc::Status, type: :model do
   context 'associations' do
-    it { should belong_to(:profile).class_name('Doc::Profile').with_foreigh_key('doc_profile_id') }
+    it { should belong_to(:profile).class_name('Doc::Profile').with_foreign_key('doc_profile_id') }
   end
 end
