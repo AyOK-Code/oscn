dcd0f23674a6b6f6df2ec26691b0d33ba57dfe69
Holden Mitchell <htmitchell90@gmail.com>
2022-04-18 15:14:06 -0500

    Fix report warrant spec

--- spec/models/report_warrants_spec.rb
+++ spec/models/report_warrants_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ReportWarrant do
+RSpec.describe ReportWarrants do
   describe '#check_view' do
     it 'yields the view when underlying data exists' do
       skip
