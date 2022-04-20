bd825336797c669bec586ea492de2806beb2c985
Joshua Williams <jwilliams@Joshuas-MacBook-Air.local>
2022-04-20 09:17:54 -0500

    added specs for case_type_spec,counsel_spec,and docket_event_spec

--- spec/models/case_type_spec.rb
+++ spec/models/case_type_spec.rb
@@ -14,17 +14,26 @@ RSpec.describe CaseType, type: :model do
   # https://betterprogramming.pub/using-environment-variables-with-rspec-unit-tests-f094b400c299
   describe '.active' do
     it 'returns case types that are included in the CASE_TYPES_ABBREVIATION ENV variable' do
-      skip
+      # get access to this envio variable and post its contents?
+      create(:case_type, abbreviation: 'CF')
+      expect(described_class.active.size).to eq(1)
     end
 
     it 'filters out case types that are not included in the CASE_TYPES_ABBREVIATION ENV variable' do
-      skip
+      create(:case_type, abbreviation: 'ZZ')
+      expect(described_class.active.size).to eq(0)
     end
   end
 
   describe '#oscn_id_mapping' do
     it 'returns a hash of with key = :abbreviation and value = :id' do
-      skip
+      # it seems that only 1 id can be associated with a certain abbreviation.
+      # if 2 case_types have same abbeviation, recent one replaces old one.
+      cassygood = create(:case_type, abbreviation: 'CF')
+      cassygood2 = create(:case_type, abbreviation: 'CM')
+      create(:case_type, abbreviation: 'ZZ')
+      expect(described_class.oscn_id_mapping.size).to eq(2)
+      expect(described_class.oscn_id_mapping.values).to include(cassygood.id, cassygood2.id)
     end
   end
 end
