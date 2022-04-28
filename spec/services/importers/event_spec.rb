5660aab4af9f6e959d03d92e2224ed980605a08a
Joshua Williams <jwilliams@Joshuas-MacBook-Air.local>
2022-04-28 10:41:41 -0500

    added importer spec and json for event

--- spec/services/importers/event_spec.rb
+++ spec/services/importers/event_spec.rb
@@ -2,8 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Importers::Event do
   describe '#perform' do
-    it 'add specs' do
-      skip
+    let(:file_path) { 'spec/fixtures/importers/event_data.json' }
+    let(:test_data) { parse_json(file_path) }
+    let(:court_case) { create(:court_case) }
+    let(:log) { ::Importers::Logger.new(court_case) }
+    context 'event does not exist' do
+      it 'creates a event' do
+        described_class.perform(test_data, court_case, log)
+
+        expect(Event.all.size).to eq(2)
+      end
+      it 'links  event to court case' do
+        described_class.perform(test_data, court_case, log)
+
+        expect(court_case.events.first.event_at).to eq(test_data.first[:date])
+      end
+    end
+
+    context 'event does  exist' do
+      it 'doesnt make duplicates' do
+        described_class.perform(test_data, court_case, log)
+
+        expect(Event.all.size).to eq(2)
+      end
+      it 'links  event to court case' do
+        described_class.perform(test_data, court_case, log)
+
+        expect(court_case.events.first.event_at).to eq(test_data.first[:date])
+      end
     end
   end
 end
