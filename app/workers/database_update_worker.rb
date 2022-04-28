d87535119f465ff927cb93a2e25f9cad03016886
Holden Mitchell <htmitchell90@gmail.com>
2022-04-28 14:05:16 -0500

    Remove throttle from database update

--- app/workers/database_update_worker.rb
+++ app/workers/database_update_worker.rb
@@ -3,9 +3,7 @@ require 'importers/court_case'
 # Ex: https://www.oscn.net/dockets/GetCaseInformation.aspx?db=tulsa&number=CF-2018-1016
 class DatabaseUpdateWorker
   include Sidekiq::Worker
-  include Sidekiq::Throttled::Worker
   sidekiq_options retry: 5, queue: :medium
-  sidekiq_throttle_as :oscn
 
   def perform(args)
     ::Importers::CourtCase.perform(args['county_id'], args['case_number'])
