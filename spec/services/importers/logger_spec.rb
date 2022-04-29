eb0a8fd5095e5dd73dd244099954b230fb9a8acb
Joshua Williams <jwilliams@Joshuas-MacBook-Air.local>
2022-04-29 10:10:03 -0500

    Added Logger spec

--- spec/services/importers/logger_spec.rb
+++ spec/services/importers/logger_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Importers::Logger do
-  describe '#perform' do
-    it 'add specs' do
-      skip
+  let(:file_path) { 'spec/fixtures/importers/count_data.json' }
+  let(:test_data) { parse_json(file_path) }
+  let(:court_case) { create(:court_case) }
+  let(:party) { create(:party, full_name: 'PIT, ANTHONY LEE') }
+  let(:log) { ::Importers::Logger.new(court_case) }
+  describe '#update_logs' do
+    it 'updates  court case log if logs are present with a log' do
+      count_data = test_data.first
+      log.create_log('counts', "#{court_case.case_number} skipped count due to missing party.", count_data)
+      log.update_logs
+      expect(court_case.logs.size).to eq(1)
+    end
+
+    it 'updates  court case log if logs are not present with nil' do
+      log.update_logs
+      expect(court_case.logs).to eq(nil)
+    end
+  end
+
+  describe '#create_log' do
+    it 'creates a log' do
+      count_data = test_data.first
+
+      log.create_log('counts', "#{court_case.case_number} skipped count due to missing party.", count_data)
+
+      expect(log.logs.size).to eq(1)
     end
   end
 end
