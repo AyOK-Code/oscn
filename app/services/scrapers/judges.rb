400fec3f3861d67aa01ea95e8167eea7a4dac884
Holden Mitchell <htmitchell90@gmail.com>
2022-05-02 14:26:03 -0500

    Add in headers to make judge update safe

--- app/services/scrapers/judges.rb
+++ app/services/scrapers/judges.rb
@@ -11,6 +11,18 @@ module Scrapers
     end
 
     def perform
+      # rubocop:disable Layout/LineLength
+      user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+      accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+      # rubocop:enable Layout/LineLength
+
+      html = HTTParty.get(base_url, headers: {
+                            'User-Agent': user_agent,
+                            'Accept-Encoding': 'gzip, deflate, br',
+                            'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
+                            Accept: accept
+                          })
+      parsed_data = Nokogiri::HTML(html.body)
       data = parsed_data.css('select[name="Judge"]')
       data.css('option').each do |o|
         j = Judge.find_or_initialize_by(oscn_id: o['value'])
