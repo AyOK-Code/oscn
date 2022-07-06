808c192c94514ca31b98fa198ebbe55536965d1e
Joshua Williams <jwilliams@9bcorp.com>
2022-07-06 15:44:35 -0500

    improved spec for new_cases_spec

--- spec/services/importers/new_cases_spec.rb
+++ spec/services/importers/new_cases_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Importers::NewCourtCase do
   describe '#perform' do
-    it 'add specs' do
-      skip
+    it 'perform correctly finds or creates and returns a new case' do
+      create(:county, name: 'Oklahoma')
+      create(:case_type, :felony)
+      create(:county, name: 'Arkansas')
+      create(:case_type, :misdemeanor)
+      file_path = 'spec/fixtures/importers/link.html'
+      html = File.open(file_path).read
+       link_html = Nokogiri::HTML.parse(html)
+       link_html = link_html.css('a').first
+
+      data = described_class.new(link_html)
+      data.perform
+      expect(data.case_number).to eq "CF-2021-489"
+      expect(data.case_types.keys.first).to eq "CF"
+      expect(data.counties.keys.first).to eq "Oklahoma"
+          
+      
     end
   end
 end
