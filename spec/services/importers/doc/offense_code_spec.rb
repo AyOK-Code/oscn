20ed3838dd7805920ccf6f0bcb368ab56929fdd5
Joshua Williams <jwilliams@9bcorp.com>
2022-07-07 13:19:03 -0500

    WIP spec improvement for offense_code

--- spec/services/importers/doc/offense_code_spec.rb
+++ spec/services/importers/doc/offense_code_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe Importers::Doc::OffenseCode do
   describe '#perform' do
     it 'imports the data' do
-      skip
+      
+     offense= create(:doc_offense_code, statute_code:300,description:'TEST',is_violent:true)
+
+     
+
+     data = allow(Importers::Doc::OffenseCode).to receive(:perform).and_return(offense)
+     binding.pry
+      
+      expect()
+      
     end
   end
 end
