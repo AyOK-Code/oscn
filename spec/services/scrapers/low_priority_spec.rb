705e094b38a733d4ce65f7a8e011d07a262c75f4
Joshua Williams <jwilliams@9bcorp.com>
2022-07-11 14:18:18 -0500

    pushed code coverage to 100%

--- spec/services/scrapers/low_priority_spec.rb
+++ spec/services/scrapers/low_priority_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe Scrapers::LowPriority do
       test_case = create(:court_case, :with_html, closed_on: '1/1/2022')
       test_case.case_html.update(scraped_at: '1/1/2022')
 
-      # data = described_class.perform
+      data = described_class.perform
+      
       expect do
         described_class.perform
       end.to change(CourtCaseWorker.jobs, :size).by(1)
+      expect(data.first.id).to eq test_case.id
     end
   end
 end
