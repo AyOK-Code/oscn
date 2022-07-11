f3ae7f64f055ab86dcf73c917b5b33eb828a9107
Joshua Williams <jwilliams@9bcorp.com>
2022-07-11 16:54:13 -0500

    got high priority to 100% coverage

--- spec/services/scrapers/high_priority_spec.rb
+++ spec/services/scrapers/high_priority_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 RSpec.describe Scrapers::HighPriority do
   describe '#perform' do
     it 'add specs' do
-        test_case = create(:court_case, :with_html, closed_on: '6/15/2022')
-        test_case.case_html.update(scraped_at: '6/15/2022')
-        false_case = create(:court_case, :with_html, closed_on: '7/11/2022')
-        false_case.case_html.update(scraped_at: '7/11/2022')
-        
-  
-        # data = described_class.perform
-        expect do
-          described_class.perform.to change(CourtCaseWorker.jobs, :size).by(1)
-        end
+      county = create(:county, name: 'Tulsa')
+      test_case = create(:court_case, closed_on: '6/15/2022', county: county)
+
+      create(:court_case, closed_on: '7/11/2022', county: county)
+
+      data = described_class.perform('Tulsa')
+      expect do
+        described_class.perform.to change(CourtCaseWorker.jobs, :size).by(1)
+      end
+      expect(data.first).to eq test_case.case_number
     end
   end
 end
