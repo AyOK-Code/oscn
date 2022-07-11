3583ac0a9d98d6a7a33b375fc98aa0b436e4162b
Joshua Williams <jwilliams@9bcorp.com>
2022-07-11 13:29:38 -0500

    high_priority_spec improved

--- spec/services/scrapers/high_priority_spec.rb
+++ spec/services/scrapers/high_priority_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe Scrapers::HighPriority do
   describe '#perform' do
     it 'add specs' do
-      skip
+        test_case = create(:court_case, :with_html, closed_on: '6/15/2022')
+        test_case.case_html.update(scraped_at: '6/15/2022')
+        false_case = create(:court_case, :with_html, closed_on: '7/11/2022')
+        false_case.case_html.update(scraped_at: '7/11/2022')
+        
+  
+        # data = described_class.perform
+        expect do
+          described_class.perform.to change(CourtCaseWorker.jobs, :size).by(1)
+        end
     end
   end
 end
