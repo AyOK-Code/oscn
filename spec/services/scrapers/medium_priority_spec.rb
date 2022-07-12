7432db1984f3a67ffdb660f1961b615002990fcd
Joshua Williams <jwilliams@9bcorp.com>
2022-07-12 13:15:00 -0500

    removed comment

--- spec/services/scrapers/medium_priority_spec.rb
+++ spec/services/scrapers/medium_priority_spec.rb
@@ -10,10 +10,9 @@ RSpec.describe Scrapers::MediumPriority do
       false_case = create(:court_case, :with_html, closed_on: '6/11/2022')
       false_case.case_html.update(scraped_at: '7/11/2022')
 
-      # data = described_class.perform
       expect do
-        described_class.perform
-      end.to change(CourtCaseWorker.jobs, :size).by(1)
+        described_class.perform.to change(CourtCaseWorker.jobs, :size).by(1)
+      end
     end
   end
 end
