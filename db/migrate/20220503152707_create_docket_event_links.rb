0b217c4b3c7f9e58d17b4261c47fa0e2dd9ff11b
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2022-07-13 15:26:50 -0500

    Update db/migrate/20220503152707_create_docket_event_links.rb

--- db/migrate/20220503152707_create_docket_event_links.rb
+++ db/migrate/20220503152707_create_docket_event_links.rb
@@ -2,9 +2,9 @@ class CreateDocketEventLinks < ActiveRecord::Migration[6.0]
   def change
     create_table :docket_event_links do |t|
       t.references :docket_event, null: false, foreign_key: true
-      t.integer :oscn_id ,null: false
-      t.string :title,null: false
-      t.string :link,null: false
+      t.integer :oscn_id, null: false
+      t.string :title, null: false
+      t.string :link, null: false
 
       t.timestamps
     end
