699ba3f00d890f1acd64c0431935f3f128a55af4
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2022-07-13 15:28:23 -0500

    Update spec/factories/docket_event_links.rb

--- spec/factories/docket_event_links.rb
+++ spec/factories/docket_event_links.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :docket_event_link do
-    docket_event { nil }
-    oscn_id { 1 }
+    docket_event
+    sequence(:oscn_id) 
     title { ['TIFF', 'PDF'].sample }
     link { Faker::Internet.url(host: 'oscn.net') }
   end
