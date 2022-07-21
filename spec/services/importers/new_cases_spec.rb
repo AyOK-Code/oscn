1c58675ddd10185799c002fe007e1d711e53c057
Joshua Williams <jwilliams@9bcorp.com>
2022-07-21 15:28:30 -0500

    added suggestions

--- spec/services/importers/new_cases_spec.rb
+++ spec/services/importers/new_cases_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Importers::NewCourtCase do
   describe '#perform' do
-    it 'perform correctly finds or creates and returns a new case' do
-      create(:county, name: 'Oklahoma')
+   
+    it 'saves to the database' do
+      county = create(:county, name: 'Oklahoma')
       create(:case_type, :felony)
       create(:county, name: 'Arkansas')
       create(:case_type, :misdemeanor)
@@ -14,9 +15,33 @@ RSpec.describe Importers::NewCourtCase do
 
       data = described_class.new(link_html)
       data.perform
-      expect(data.case_number).to eq 'CF-2021-489'
-      expect(data.case_types.keys.first).to eq 'CF'
-      expect(data.counties.keys.first).to eq 'Oklahoma'
+      oscn_id = data.params['casemasterID']
+      court_case = CourtCase.find_by(county_id: county.id, oscn_id: oscn_id)
+
+      expect(court_case.case_number).to eq 'CF-2021-489'
+      expect(court_case.case_number[0..1]).to eq 'CF'
+      expect(court_case.county.name).to eq 'Oklahoma'
+
+    end
+
+    it ' does not duplicate' do
+      county = create(:county, name: 'Oklahoma')
+      create(:case_type, :felony)
+      create(:county, name: 'Arkansas')
+      create(:case_type, :misdemeanor)
+      file_path = 'spec/fixtures/importers/link.html'
+      html = File.read(file_path)
+      link_html = Nokogiri::HTML.parse(html)
+      link_html = link_html.css('a').first
+
+      data = described_class.new(link_html)
+      data.perform
+     
+      false_data= described_class.new(link_html)
+      false_data.perform
+
+      expect(CourtCase.all.size).to eq 1
+  
     end
   end
 end
