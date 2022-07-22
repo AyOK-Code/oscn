5088509ba5eed6cfc0e04ff643bd30ac0d5f2845
Joshua Williams <jwilliams@9bcorp.com>
2022-07-22 11:45:22 -0500

    refactored spec

--- spec/services/importers/new_cases_spec.rb
+++ spec/services/importers/new_cases_spec.rb
@@ -16,15 +16,14 @@ RSpec.describe Importers::NewCourtCase do
       oscn_id = data.params['casemasterID']
       court_case = CourtCase.find_by(county_id: county.id, oscn_id: oscn_id)
 
-      
       expect(court_case.case_number).to eq 'CF-2021-489'
       expect(court_case.case_number[0..1]).to eq 'CF'
       expect(court_case.county.name).to eq 'Oklahoma'
     end
 
     it ' does not duplicate' do
-    county=  create(:county, name: 'Oklahoma')
-     create(:court_case, :felony , county_id: county.id, oscn_id: 3946802)
+      county = create(:county, name: 'Oklahoma')
+      create(:court_case, :felony, county_id: county.id, oscn_id: 3_946_802)
       file_path = 'spec/fixtures/importers/link.html'
       html = File.read(file_path)
       link_html = Nokogiri::HTML.parse(html)
@@ -33,7 +32,6 @@ RSpec.describe Importers::NewCourtCase do
       data = described_class.new(link_html)
       data.perform
 
-      
       expect(CourtCase.all.size).to eq 1
     end
   end
