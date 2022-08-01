2bc13c10118910cfeaeb04efb25e05f3c9697bdc
sabrinaleggett <sabrina.gelbart@gmail.com>
2022-08-01 15:13:57 -0500

    Add spec for rake tasks and make docket text check case insensitive

---
+++ spec/lib/tasks/update_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'rake'
+
+Rails.application.load_tasks
+
+RSpec.describe 'update' do
+  describe 'is_error' do
+    after(:each) do
+      Rake::Task['update:is_error'].reenable
+    end
+
+    context 'when there are error cases and valid cases' do
+      let!(:empty_error_case) { create(:court_case) }
+      let!(:docket_error_case) do
+        create(:court_case, docket_events: [create(:docket_event, description: 'CASE FILED IN ERROR')])
+      end
+      let!(:valid_case) { create(:court_case, :with_docket_event) }
+
+      it 'it updates is_error flag on error cases only' do
+        Rake::Task['update:is_error'].invoke
+        expect(empty_error_case.reload.is_error).to be true
+        expect(docket_error_case.reload.is_error).to be true
+        expect(valid_case.reload.is_error).to be false
+      end
+    end
+  end
+end
