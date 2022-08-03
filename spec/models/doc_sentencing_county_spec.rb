e8e468557be56c65b7ec1111c21f85d4080a878f
Joshua Williams <jwilliams@9bcorp.com>
2022-08-03 14:14:01 -0500

    added association check in doc_sentencing_county_spec

--- spec/models/doc_sentencing_county_spec.rb
+++ spec/models/doc_sentencing_county_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe DocSentencingCounty, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+  describe 'associations' do
+    it { should belong_to(:county).class_name('County').optional }
+  end
 end
