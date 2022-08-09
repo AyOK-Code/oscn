a9c61aba2855d5e1f96e0d5b794602fe6356a50b
Sabrina Leggett <sabrina.gelbart@gmail.com>
2022-08-09 10:17:42 -0500

    ARNALL-222 Orchestrate party import by priority (#93)
    * confirmed working
    need to add tests
    
    * added specs
    
    * rm recent parties
    rm comments
    
    * add model specs
    adjust limits
    
    * rubocop
    
    * fix to be select instead of pluck
    
    * spec fix
    
    * Update spec/services/scrapers/parties/high_priority.rb
    
    * Update spec/services/scrapers/parties/high_priority.rb
    
    * fix bug

---
+++ app/services/scrapers/parties/high_priority.rb
@@ -0,0 +1,25 @@
+module Scrapers
+  module Parties
+    class HighPriority
+      def self.perform
+        new.perform
+      end
+
+      def perform
+        parties_oscn_ids = ::Party.without_html.limit(parties_high_count).pluck(:oscn_id)
+
+        bar = ProgressBar.new(parties_oscn_ids.count)
+        puts "#{parties_oscn_ids.count} are missing html"
+
+        parties_oscn_ids.each do |oscn_id|
+          PartyWorker.perform_async(oscn_id)
+          bar.increment!
+        end
+      end
+
+      def parties_high_count
+        ENV.fetch('PARTIES_HIGH_PRIORITY', 5_000).to_i
+      end
+    end
+  end
+end
