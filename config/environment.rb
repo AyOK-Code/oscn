fa12f74575b5f6f80a2ae1519031c54512bfbd9f
sabrinaleggett <sabrina.gelbart@gmail.com>
2022-08-25 13:01:25 -0500

    rm comments

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-#
-# if ENV['PROXY_URL']
-#   HTTParty::Basement.http_proxy(ENV['PROXY_URL'], ENV.fetch['PROXY_PORT'], nil, nil)
-# end
-#
