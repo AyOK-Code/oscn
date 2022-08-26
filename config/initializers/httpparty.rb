ef582f6cea795b846dd5355ec0caf7a362baf569
sabrinaleggett <sabrina.gelbart@gmail.com>
2022-08-26 09:28:01 -0500

    comments

--- config/initializers/httpparty.rb
+++ config/initializers/httpparty.rb
@@ -1,2 +1,7 @@
 require 'openssl'
+
+# There seems to be issues with oscn's ssl certificate
+# this only popped up when we added proxy server but *seems* to be happening without the proxy server
+# happens intermittently on both heroku and locally
+# todo: figure out how we can remove this?
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
