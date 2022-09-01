d6a72995f7d46bee5afd9a110ebac9df36040834
Joshua Williams <jwilliams@9bcorp.com>
2022-09-01 16:23:27 -0500

    forogt spec

--- spec/missing_cases_spec.rb
+++ spec/missing_cases_spec.rb
@@ -5,14 +5,18 @@ Rails.application.load_tasks
 
 RSpec.describe 'save' do
   describe 'missing_cases' do
-    let!(:court_case1) { create(:court_case) }
-    let!(:court_case2) { create(:court_case) }
-    let!(:court_case3) { create(:court_case) }
-    let!(:court_case4) { create(:court_case) }
+    ENV['COUNTIES'] = 'Tulsa'
+    let!(:county) { create(:county, name: ENV['COUNTIES']) }
+    let!(:case_type) { create(:case_type, name: 'Criminal Felony', abbreviation: 'CF') }
+    let!(:court_case1) { create(:court_case, case_number: 'CF-2012-1') }
+    let!(:court_case2) { create(:court_case, case_number: 'CF-2012-2') }
+    let!(:court_case3) { create(:court_case, case_number: 'CF-2012-3') }
+    let!(:court_case4) { create(:court_case, case_number: 'CF-2012-5') }
 
     it 'finds missing task' do
-      ENV['COUNTIES'] = 'Tulsa'
       Rake::Task['save:missing_cases'].invoke
+
+      expect(CourtCase.where(case_number: 'CF-2012-4').exists?).to eq true
     end
   end
 end
