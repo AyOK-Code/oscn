2f084fcce74c6332e3415b49bb68430ebd9e3be2
Joshua Williams <jwilliams@9bcorp.com>
2022-09-14 11:01:11 -0500

    changes before  rollback

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -1,5 +1,5 @@
 class Roster < ApplicationRecord
-  has_many :booking, foreign_key: 'roster_id'
+  has_many :bookings,class_name: 'OkcBlotter::Booking', foreign_key: 'roster_id'
   has_many :case_parties, foreign_key: 'roster_id'
-  has_many :doc_profiles, foreign_key: 'roster_id'
+  has_many :doc_profiles, class_name: 'Doc::Profile',foreign_key: 'roster_id'
 end
--- spec/models/roster_spec.rb
+++ spec/models/roster_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Roster, type: :model do
   describe 'associations' do
-    it { should have_many(:booking)}
-    it { should have_many(:case_parties)}
-    it { should have_many(:doc_profiles)}
+    it { should have_many(:bookings).class_name('OkcBlotter::Booking').with_foreign_key('roster_id').optional}
+    it { should have_many(:case_parties).class_name('CaseParty').with_foreign_key('roster_id').optional}
+    it { should have_many(:doc_profiles).class_name('Doc::Profile').with_foreign_key('roster_id').optional}
   end
   
 end
