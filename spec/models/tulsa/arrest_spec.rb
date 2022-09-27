f6e8689265d1cce654ae5da018f853578e668da4
Joshua Williams <jwilliams@9bcorp.com>
2022-09-27 09:51:16 -0500

    removing optional

--- spec/models/tulsa/arrest_spec.rb
+++ spec/models/tulsa/arrest_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe TulsaBlotter::Arrest, type: :model do
   end
 
   describe 'associations' do
-    it { should belong_to(:inmate).class_name('TulsaBlotter::Inmate').optional }
+    it { should belong_to(:inmate).class_name('TulsaBlotter::Inmate') }
     it { should have_many(:offenses).class_name('TulsaBlotter::Offense') }
   end
 end
