ccc7ccd7a561c20c6de8106093dffe08b7b586d7
Joshua Williams <102045982+AspiringDev51@users.noreply.github.com>
2022-10-04 15:31:46 -0500

    added where_not clause to exclude parties with no oscn_id in low and high priority party scrapers (#120)

--- app/services/scrapers/parties/high_priority.rb
+++ app/services/scrapers/parties/high_priority.rb
@@ -6,7 +6,7 @@ module Scrapers
       end
 
       def perform
-        parties_oscn_ids = ::Party.without_html.limit(parties_high_count).pluck(:oscn_id)
+        parties_oscn_ids = ::Party.without_html.where.not(oscn_id: nil).limit(parties_high_count).pluck(:oscn_id)
 
         bar = ProgressBar.new(parties_oscn_ids.count)
         puts "#{parties_oscn_ids.count} are missing html"
