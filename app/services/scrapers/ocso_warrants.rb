6fbd28b0f2e281795a61641443b36e88dd7c7f6e
Joshua Williams <jwilliams@9bcorp.com>
2022-10-25 17:04:12 -0500

    added parser, got single page of information into a hash

--- app/services/scrapers/ocso_warrants.rb
+++ app/services/scrapers/ocso_warrants.rb
@@ -2,7 +2,32 @@ module Scrapers
     class OcsoWarrants
 
         def self.perform
+            
             html = OcsoScraper::Crawler.request
+            parsed_html = OcsoScraper::CrawlerParser.parser(html)
+          #  puts parsed_html
+            
+            data = parsed_html.css('td font').children
+
+            warrants = []
+
+            data.each_slice(6) do |l,f,m,d,c,o|
+                extra = c.text.split('   ')
+                warrant= {
+                    last:l.text,
+                    first:f.text,
+                    middle:m.text,
+                    dob:d.text,
+                    case:extra[0],
+                    bond:extra[1],
+                    issued:extra[2],
+                    offense:o.text    
+                }
+                warrants << warrant
+            end
+            binding.pry
+            
+            
         end
 
     end
