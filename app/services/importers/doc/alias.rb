c19a99c5342e8cbd8dcf0438c6b74e5e15fe384d
Joshua Williams <jwilliams@9bcorp.com>
2022-12-08 17:27:33 -0600

    edited alias and profile  doc importer to use upsert_all, added migration for combined index field and to set default date for created and updated for doc importers

--- app/services/importers/doc/alias.rb
+++ app/services/importers/doc/alias.rb
@@ -9,6 +9,7 @@ module Importers
         @field_pattern = "A#{fields.join('A')}"
         @bar = ProgressBar.new(@file.body.string.split("\r\n").size)
         @doc_mapping = ::Doc::Profile.pluck(:doc_number, :id).to_h
+        @aliases = []
       end
 
       def perform
@@ -16,24 +17,26 @@ module Importers
           bar.increment!
           data = line.unpack(field_pattern).map(&:squish)
           profile_id = doc_mapping[data[0].to_i]
+
           next if profile_id.blank?
 
-          save_alias(data)
+          @aliases << save_alias(data)
         end
+
+        ::Doc::Alias.upsert_all(@aliases)
       end
 
       private
 
       def save_alias(data)
-        doc_alias = ::Doc::Alias.find_or_initialize_by(
+        {
           doc_profile_id: doc_mapping[data[0].to_i],
           doc_number: data[0],
           last_name: data[1],
           first_name: data[2],
           middle_name: data[3],
           suffix: data[4]
-        )
-        doc_alias.save!
+        }
       end
     end
   end
