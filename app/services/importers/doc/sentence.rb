0f5ac8377f0fc7c2caf99b84e708afa4d176f2b2
Joshua Williams <jwilliams@9bcorp.com>
2022-12-09 15:58:01 -0600

    offense_code and sentence importer done

--- app/services/importers/doc/sentence.rb
+++ app/services/importers/doc/sentence.rb
@@ -4,6 +4,7 @@ module Importers
       attr_accessor :file, :fields, :field_pattern, :bar, :doc_mapping, :sentence_mapping
 
       def initialize(dir)
+        @sentences = []
         @file = Bucket.new.get_object("doc/#{dir}/Vendor_sentence_Extract_Text.dat")
         @fields = [11, 20, 20, 40, 40, 8, 40, 13, 13]
         @field_pattern = "A#{fields.join('A')}"
@@ -16,8 +17,11 @@ module Importers
         file.body.string.split("\r\n").each do |line|
           bar.increment!
           data = line.unpack(field_pattern).map(&:squish)
-          find_and_save_sentence(data)
+
+          @sentences << find_and_save_sentence(data)
         end
+        @sentences.compact!
+        ::Doc::Sentence.upsert_all(@sentences)
       end
 
       private
@@ -29,29 +33,21 @@ module Importers
         save_sentence(data)
       end
 
-      def find_sentence(data)
-        ::Doc::Sentence.find_or_initialize_by(doc_profile_id: doc_mapping[data[0].to_i], sentence_id: data[1])
-      end
-
       def save_sentence(data)
-        sentence = find_sentence(data)
-
-        sentence.assign_attributes(
-          {
-            sentencing_county: data[4],
-            consecutive_to_sentence_id: data[2],
-            js_date: data[5].present? ? Date.parse(data[5]) : '',
-            crf_number: data[6],
-            statute_code: data[3],
-            incarcerated_term_in_years: data[7].to_f,
-            probation_term_in_years: data[8].to_f,
-            is_death_sentence: data[7].to_i == 9999,
-            is_life_sentence: data[7].to_i == 8888,
-            is_life_no_parole_sentence: data[7].to_i == 7777
-          }
-        )
-
-        sentence.save!
+        {
+          doc_profile_id: doc_mapping[data[0].to_i],
+          sentence_id: data[1],
+          sentencing_county: data[4],
+          consecutive_to_sentence_id: data[2],
+          js_date: data[5].present? ? Date.parse(data[5]) : '',
+          crf_number: data[6],
+          statute_code: data[3],
+          incarcerated_term_in_years: data[7].to_f,
+          probation_term_in_years: data[8].to_f,
+          is_death_sentence: data[7].to_i == 9999,
+          is_life_sentence: data[7].to_i == 8888,
+          is_life_no_parole_sentence: data[7].to_i == 7777
+        }
       end
     end
   end
