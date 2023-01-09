c06aa7206083257b144db8f07c95c8ddcc8d4658
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-09 10:40:38 -0800

    add raygun

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,7 +1,9 @@
-Raygun.setup do |config|
-  config.api_key = "xXqgNvYI0fmPXWmKbfXLg"
-  config.filter_parameters = Rails.application.config.filter_parameters
+if ENV.fetch('RAYGUN_API_KEY', false)
+  Raygun.setup do |config|
+    config.api_key = ENV['RAYGUN_API_KEY']
+    config.filter_parameters = Rails.application.config.filter_parameters
 
-  # The default is Rails.env.production?
-  # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+    # The default is Rails.env.production?
+    # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
+  end
 end
