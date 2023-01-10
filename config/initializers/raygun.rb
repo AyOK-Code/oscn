cd7f5d9cfa4906f95df7305b7ddd5aa38c0002ad
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-10 14:19:19 -0800

    disable

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -5,6 +5,6 @@ if ENV.fetch('RAYGUN_API_KEY', false)
     config.filter_parameters = Rails.application.config.filter_parameters
 
     # The default is Rails.env.production?
-    config.enable_reporting = true
+    # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
   end
 end
