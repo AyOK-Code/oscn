3752c3c5401278f568bc32e20ce91da1bc75846c
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-11 10:53:50 -0800

    wip throttled

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,11 +8,18 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
 end
 
+puts "inside initializer"
 # Create oscn throttling strategy
+OKLAHOMA_DAY_START = Time.parse("02:00 pm")
+OKLAHOMA_DAY_END = Time.parse("10:00 pm")
 Sidekiq::Throttled::Registry.add(:oscn, **{
-  threshold: { limit: ENV.fetch('OSCN_THROTTLE', 6).to_i, period: 1.minute },
-  concurrency: { limit: ENV.fetch('OSCN_CONCURRENCY', 1).to_i }
+  threshold: {
+    :limit => ->(_) {
+      puts "inside registry threshold"
+      Time.now.between?(OKLAHOMA_DAY_START, OKLAHOMA_DAY_END) ? ENV.fetch('OSCN_THROTTLE', 6).to_i : 1_000
+    },
+    :period => 1.minute
+  }
 })
-
 
 Sidekiq::Throttled.setup!
