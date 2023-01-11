5d2389ee33209de4c5a48376525a13d3c245e312
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-11 09:18:47 -0800

    fix raygun

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -1,16 +1,18 @@
 # Override Rake::Task to include global exception handling
-
 if !Rails.env.test?
-  module Rake
-    class Task
-      alias :orig_execute :execute
-      def execute(args=nil)
-        orig_execute(args)
-      rescue Exception => ex
-        raise ex if [Interrupt, SystemExit, SignalException].include?(ex.class)
-        Raygun.track_exception(ex)
-        raise ex
+  begin
+    module Rake
+      class Task
+        alias :orig_execute :execute
+        def execute(args=nil)
+          orig_execute(args)
+        rescue Exception => ex
+          raise ex if [Interrupt, SystemExit, SignalException].include?(ex.class)
+          Raygun.track_exception(ex)
+          raise ex
+        end
       end
     end
+  rescue
   end
 end
