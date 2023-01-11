fe1c525c2e2d861e8b4642004ab422bd64d4d2bd
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-11 07:43:14 -0800

    rm binding.pry

--- config/initializers/task.rb
+++ config/initializers/task.rb
@@ -1,6 +1,5 @@
 # Override Rake::Task to include global exception handling
 
-binding.pry
 if !Rails.env.test?
   module Rake
     class Task
@@ -8,7 +7,6 @@ if !Rails.env.test?
       def execute(args=nil)
         orig_execute(args)
       rescue Exception => ex
-        binding.pry
         raise ex if [Interrupt, SystemExit, SignalException].include?(ex.class)
         Raygun.track_exception(ex)
         raise ex
