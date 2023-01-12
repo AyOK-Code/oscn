cba20559c5865068c30adc157883414370dd562b
Joshua Williams <jwilliams@9bcorp.com>
2023-01-12 15:39:58 -0600

    rubocop

--- spec/factories/parcels.rb
+++ spec/factories/parcels.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :parcel do
-    geoid20 { Faker::String.random(length: 4)}
+    geoid20 { Faker::String.random(length: 4) }
     zip { Faker::Address.zip_code }
-    tract { Faker::Lorem.word  }
+    tract { Faker::Lorem.word }
     block { Faker::Lorem.word }
     lat { Faker::Address.latitude }
     long { Faker::Address.longitude }
