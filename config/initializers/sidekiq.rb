59a010f2f8cbac8fad4c947d317f9b07f87bda1f
Sabrina Leggett <sabrina.gelbart@gmail.com>
2023-01-11 17:27:43 -0800

    Sidekiq Throttle business hours only and additional Raygun Fixes (#144)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,11 +8,16 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
 end
 
-# Create oscn throttling strategy
+# Throttle OSCN requests during business hours
+OKLAHOMA_DAY_START = Time.parse("02:00 pm UTC")
+OKLAHOMA_DAY_END = Time.parse("10:00 pm UTC")
 Sidekiq::Throttled::Registry.add(:oscn, **{
-  threshold: { limit: ENV.fetch('OSCN_THROTTLE', 6).to_i, period: 1.minute },
-  concurrency: { limit: ENV.fetch('OSCN_CONCURRENCY', 1).to_i }
+  threshold: {
+    :limit => ->(*_) {
+      Time.now.utc.between?(OKLAHOMA_DAY_START, OKLAHOMA_DAY_END) ? ENV.fetch('OSCN_THROTTLE', 60).to_i : 1_000
+    },
+    :period => 1.minute
+  }
 })
-
 
 Sidekiq::Throttled.setup!
