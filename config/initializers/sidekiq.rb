7f1d6725a35f7850d4dafc9837abf746f9f32b78
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-01-11 17:22:37 -0800

    throttle during business hours

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,15 +8,13 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://127.0.0.1:6379' }
 end
 
-puts "inside initializer"
-# Create oscn throttling strategy
-OKLAHOMA_DAY_START = Time.parse("02:00 pm")
-OKLAHOMA_DAY_END = Time.parse("10:00 pm")
+# Throttle OSCN requests during business hours
+OKLAHOMA_DAY_START = Time.parse("02:00 pm UTC")
+OKLAHOMA_DAY_END = Time.parse("10:00 pm UTC")
 Sidekiq::Throttled::Registry.add(:oscn, **{
   threshold: {
-    :limit => ->(_) {
-      puts "inside registry threshold"
-      Time.now.between?(OKLAHOMA_DAY_START, OKLAHOMA_DAY_END) ? ENV.fetch('OSCN_THROTTLE', 6).to_i : 1_000
+    :limit => ->(*_) {
+      Time.now.utc.between?(OKLAHOMA_DAY_START, OKLAHOMA_DAY_END) ? ENV.fetch('OSCN_THROTTLE', 60).to_i : 1_000
     },
     :period => 1.minute
   }
