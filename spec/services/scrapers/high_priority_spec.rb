4ccdc6f2831e1301777580f4a6c16680ea66b135
Joshua Williams <jwilliams@9bcorp.com>
2023-03-29 15:13:04 -0500

    added before statements to specs, but now other tests are failing

--- spec/services/scrapers/high_priority_spec.rb
+++ spec/services/scrapers/high_priority_spec.rb
@@ -4,27 +4,29 @@ Sidekiq::Testing.fake!
 
 RSpec.describe Scrapers::HighPriority do
   describe '#perform' do
-    let(:county) { create(:county, name: 'Tulsa') }
     # let(:test_case) {create(:court_case, closed_on: 7.days.ago, county: county)}
+    before(:all) do
+      @county =  create(:county, name: 'Tulsa')
+      @test_case = create(:court_case, closed_on: 7.days.ago, county: @county)
+      @data = nil
+    end
 
     it 'adds jobs to the CourtCaseWorker' do
-      test_case = create(:court_case, closed_on: 7.days.ago, county: county)
+      create(:court_case, closed_on: 2.days.ago, county: @county)
 
-      create(:court_case, closed_on: 2.days.ago, county: county)
-
-      data = described_class.perform('Tulsa')
+      @data = described_class.perform('Tulsa')
 
       expect do
         described_class.perform.to change(CourtCaseWorker.jobs, :size).by(1)
       end
 
-      expect(data.first).to eq test_case.case_number
+      expect(@data.first).to eq @test_case.case_number
     end
 
     it 'doesnt add duplicate job to queue' do
-      create(:court_case, closed_on: 7.days.ago, county: county)
+      create(:court_case, closed_on: 7.days.ago, county: @county)
 
-      create(:court_case, closed_on: 2.days.ago, county: county)
+      create(:court_case, closed_on: 2.days.ago, county: @county)
       described_class.perform('Tulsa')
 
       expect do
--- spec/services/scrapers/low_priority_spec.rb
+++ spec/services/scrapers/low_priority_spec.rb
@@ -4,25 +4,19 @@ Sidekiq::Testing.fake!
 
 RSpec.describe Scrapers::LowPriority do
   describe '#perform' do
+    before(:all) do
+      @test_case = create(:court_case, :with_html, closed_on: 7.months.ago)
+      @test_case.case_html.update(scraped_at: 7.months.ago)
+      @data = nil
+    end
     it 'adds jobs to the CourtCaseWorker' do
-      test_case = create(:court_case, :with_html, closed_on: 7.months.ago)
-      test_case.case_html.update(scraped_at: 7.months.ago)
-      data = nil
-
       expect do
-        data = described_class.perform
+        @data = described_class.perform
       end.to change(CourtCaseWorker.jobs, :size).by(1)
 
-      expect do
-        described_class.perform
-      end.to change(CourtCaseWorker.jobs, :size).by(0)
-
-      expect(data.first.id).to eq test_case.id
+      expect(@data.first.id).to eq @test_case.id
     end
     it 'doesnt add duplicates to worker' do
-      test_case = create(:court_case, :with_html, closed_on: 7.months.ago)
-      test_case.case_html.update(scraped_at: 7.months.ago)
-
       described_class.perform
       expect do
         described_class.perform
