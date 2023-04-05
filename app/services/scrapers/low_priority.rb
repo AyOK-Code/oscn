ea37d4b4154050c40e1f9d580491108a6dac5478
Sabrina Leggett <sabrina.gelbart@gmail.com>
2023-04-05 10:49:39 -0500

    Update app/services/scrapers/low_priority.rb

--- app/services/scrapers/low_priority.rb
+++ app/services/scrapers/low_priority.rb
@@ -19,7 +19,7 @@ module Scrapers
         county = c.county
 
         case_number = c.case_number
-        court_case = ::CourtCase.find_by!(county_id: c.county_id, case_number: c.case_number)
+        court_case = ::CourtCase.find_by!(county_id: county.id, case_number: c.case_number)
 
         next if court_case.enqueued
 
