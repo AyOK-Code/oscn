d4067a35ff59a8c4b8c8332fac55b49dad2c37ff
Sabrina Leggett <sabrina.gelbart@gmail.com>
2023-04-05 10:46:27 -0500

    Update app/services/scrapers/high_priority.rb
    Co-authored-by: Holden Mitchell <holdenmitchell@users.noreply.github.com>

--- app/services/scrapers/high_priority.rb
+++ app/services/scrapers/high_priority.rb
@@ -20,7 +20,7 @@ module Scrapers
       cases.each do |case_number|
         court_case = ::CourtCase.find_by!(county_id: @county.id, case_number: case_number)
 
-        next unless court_case.enqueued == false
+        next if court_case.enqueued
 
         court_case.update(enqueued: true)
         CourtCaseWorker
