976a0a0ec896dbbe59eb842fd2265259a5cf4218
Joshua Williams <jwilliams@9bcorp.com>
2023-04-06 17:32:05 -0500

    made extra it block but getting database error, says oscn_test database does not exist

--- spec/lib/tasks/roster_spec.rb
+++ spec/lib/tasks/roster_spec.rb
@@ -4,7 +4,7 @@ require 'rake'
 Rails.application.load_tasks
 
 RSpec.describe 'roster' do
-  let!(:test_profile) { create(:doc_profile, roster: nil) }
+  let!(:test_profile) { create(:doc_profile, roster: nil ,first_name:'Bilbo',middle_name:'G',last_name: 'Baggins',birth_date:'2022-01-24',sex:'male',race:'white') }
   let!(:test_roster) { create(:roster) }
   let!(:false_profile) { create(:doc_profile, roster: test_roster) }
   context 'There are profiles with roster ids and some without' do
@@ -15,6 +15,19 @@ RSpec.describe 'roster' do
       expect(test_profile.roster).not_to  be nil
       expect(false_profile.roster_id).to  be test_roster.id
       expect(Doc::Profile.where(roster_id: nil).count).to be 0
+    end
+    it 'shares the correct fields with doc_profiles' do
+      Rake::Task['roster:empty_doc_profiles'].invoke
+      test_roster = test_profile.roster
+      expect(test_roster.first_name).to  be test_profile.first_name
+      expect(test_roster.last_name).to  be test_profile.last_name
+      expect(test_roster.middle_name).to  be test_profile.middle_name
+      expect(test_roster.birth_month).to  be test_profile.birth_date.month
+      expect(test_roster.birth_day).to  be test_profile.birth_date.day
+      expect(test_roster.birth_year).to  be test_profile.birth_date.year
+      expect(test_roster.sex).to  be test_profile.sex
+      expect(test_roster.race).to  be test_profile.race
+   
     end
   end
 end
