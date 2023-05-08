3ccd776f40f1d43b78466781d1f2bcbc45bcee6e
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-05-08 10:14:17 -0400

    Committing tests

--- app/services/scrapers/doc.rb
+++ app/services/scrapers/doc.rb
@@ -16,10 +16,24 @@ module Scrapers
       Zip::InputStream.open(StringIO.new(input)) do |io|
         while entry = io.get_next_entry
           content = io.read
-          filename = entry.name
+          filename = entry.name.downcase
           Bucket.new.put_object("doc/#{@dir}/#{filename}", content)
         end
       end
+    end
+
+    def validate_schema_from_readme
+      expected_readme = File.read('lib/agencies_mapped.csv')
+      actual_readme = Bucket.new.get_object("doc/#{dir}/ReadMe.txt")
+      diff = Diffy::Diff.new(expected_readme, actual_readme)
+      return true unless diff
+
+      raise StandardError, <<-TXT
+        Downloaded ReadMe.txt does not match expected ReadMe.txt.
+        Please ensure no schema changes were made and update the supported_version_ReadMe.txt file. 
+
+        Diff Was:
+        #{diff}TXT
     end
 
     def download_link
