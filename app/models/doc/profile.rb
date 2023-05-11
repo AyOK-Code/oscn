ac75af98a65e903628ffbb5b1f1d7d11dad592fd
sabrinaleggett <sabrina.gelbart@gmail.com>
2023-05-11 10:24:54 -0500

    add todo

--- app/models/doc/profile.rb
+++ app/models/doc/profile.rb
@@ -4,6 +4,7 @@ class Doc::Profile < ApplicationRecord
   has_many :aliases, class_name: 'Doc::Alias', foreign_key: 'doc_profile_id', dependent: :destroy
   has_many :statuses, class_name: 'Doc::Status', foreign_key: 'doc_profile_id', dependent: :destroy
 
+  # TODO: move this (and all other doc validation) to the database level since we're doing upsert_all
   validates :last_name, :birth_date, :doc_number, :status, :sex, presence: true
 
   enum status: [:active, :inactive]
