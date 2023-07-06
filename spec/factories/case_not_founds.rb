0cabde73ab2c118113e6e4a05dc5c4c0b13bad37
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-07-06 14:26:43 -0500

    Appease the cop

--- spec/factories/case_not_founds.rb
+++ spec/factories/case_not_founds.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :case_not_found do
     county { nil }
+<<<<<<< Updated upstream
     case_number { "MyString" }
+=======
+    case_number { 'MyString' }
+>>>>>>> Stashed changes
   end
 end
