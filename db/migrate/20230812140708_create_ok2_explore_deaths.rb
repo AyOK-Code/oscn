1559e8ded3d0f80b692c3be2d2e96223bf705e0c
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-08-12 09:10:33 -0500

    Add validations and associations

--- db/migrate/20230812140708_create_ok2_explore_deaths.rb
+++ db/migrate/20230812140708_create_ok2_explore_deaths.rb
@@ -1,12 +1,12 @@
 class CreateOk2ExploreDeaths < ActiveRecord::Migration[7.0]
   def change
     create_table :ok2_explore_deaths do |t|
-      t.string :last_name
-      t.string :first_name
+      t.string :last_name, null: false
+      t.string :first_name, null: false
       t.string :middle_name
-      t.integer :sex
+      t.integer :sex, null: false
       t.references :county, null: false, foreign_key: true
-      t.datetime :death_on
+      t.datetime :death_on, null: false
 
       t.timestamps
     end
--- spec/models/ok2_explore/death_spec.rb
+++ spec/models/ok2_explore/death_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Ok2Explore::Death, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:county) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should define_enum_for(:sex) }
+    it { should validate_presence_of(:death_on) }
+  end
 end
