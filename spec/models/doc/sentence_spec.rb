a24924a0d230665a89a56a4d0dacbbba9d3d3cc4
Joshua Williams <jwilliams@9bcorp.com>
2023-09-01 10:25:39 -0500

    rubocop but other test failing that wasnt before, was not edited

--- spec/models/doc/sentence_spec.rb
+++ spec/models/doc/sentence_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe Doc::Sentence, type: :model do
     context 'when crf_number matches' do
       context 'when format ^[A-Za-z]{2}[0-9]{5,}' do
         it 'parses the crf_number' do
-          doc_sentence = create(:doc_sentence, crf_number: 'CF94008641', 
-          doc_profile_id: @doc_profile.id, sentence_id: 1,
+          doc_sentence = create(:doc_sentence, crf_number: 'CF94008641',
+                                               doc_profile_id: @doc_profile.id, sentence_id: 1,
                                                id: 1)
           doc_sentence.reload
           expect(doc_sentence.clean_case_number).to eq 'CF-1994-8641'
@@ -22,8 +22,8 @@ RSpec.describe Doc::Sentence, type: :model do
 
       context 'when format ^[A-Za-z]{2}-[0-9]{2}-[0-9]{1,}' do
         it 'parses the crf_number' do
-          doc_sentence = create(:doc_sentence, crf_number: 'CF-94-8641', 
-          doc_profile_id: @doc_profile.id, sentence_id: 2,
+          doc_sentence = create(:doc_sentence, crf_number: 'CF-94-8641',
+                                               doc_profile_id: @doc_profile.id, sentence_id: 2,
                                                id: 2)
           doc_sentence.reload
           expect(doc_sentence.clean_case_number).to eq 'CF-1994-8641'
@@ -33,7 +33,7 @@ RSpec.describe Doc::Sentence, type: :model do
       context 'when format ^[A-Za-z]{2}-[0-9]{4}-[0-9]{1,}' do
         it 'parses the crf_number' do
           doc_sentence = create(:doc_sentence, crf_number: 'CF-1994-8641',
-           doc_profile_id: @doc_profile.id, sentence_id: 3,
+                                               doc_profile_id: @doc_profile.id, sentence_id: 3,
                                                id: 3)
           doc_sentence.reload
           expect(doc_sentence.clean_case_number).to eq 'CF-1994-8641'
@@ -42,8 +42,8 @@ RSpec.describe Doc::Sentence, type: :model do
     end
     context 'when crf_number does not match' do
       it 'is null' do
-        doc_sentence = create(:doc_sentence, crf_number: 'ABC-19991-0', 
-        doc_profile_id: @doc_profile.id, sentence_id: 4,
+        doc_sentence = create(:doc_sentence, crf_number: 'ABC-19991-0',
+                                             doc_profile_id: @doc_profile.id, sentence_id: 4,
                                              id: 4)
         doc_sentence.reload
         expect(doc_sentence.clean_case_number).to be nil
