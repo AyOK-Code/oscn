77035d62e89b75e4f2476bf36a9f7a44353143a3
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-10-19 11:47:50 -0500

    Update app/services/importers/party.rb

--- app/services/importers/party.rb
+++ app/services/importers/party.rb
@@ -82,7 +82,7 @@ module Importers
       begin
         party_type_id = find_or_create_party_type(party_data[:party_type].downcase)
       rescue NoMethodError
-        logs.create_log('parties', "#{court_case.case_number}: error when creating the party, likely null", party_data)
+        logs.create_log('parties', "#{court_case.case_number}: error when creating the party", party_data)
       end
 
       return if text_only_party_exists?(full_name, party_type_id)
