e36f5be6803a1e9f7f8e305cf5fccfa56d548be7
Joshua Williams <jwilliams@9bcorp.com>
2023-10-20 14:38:20 -0500

    improved error

--- app/services/importers/party_address.rb
+++ app/services/importers/party_address.rb
@@ -22,7 +22,7 @@ module Importers
       begin
         create_party_address(address_columns)
       rescue StandardError => e
-        Raygun.track_exception(e, custom_data: { error_type: 'Data Error' })
+        Raygun.track_exception(e, custom_data: { error_type: 'Data Error', data_content: address_columns })
       end
     end
 
