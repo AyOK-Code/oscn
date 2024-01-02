d17444c304b0106d26bd58995053f870d9d452e3
Joshua Williams <jwilliams@9bcorp.com>
2024-01-02 12:07:57 -0600

    importer done

--- app/services/importers/structure_fire.rb
+++ app/services/importers/structure_fire.rb
@@ -3,19 +3,22 @@ module Importers
   # Pulls accurate data from the OK Bar Association
   # TODO: Figure out refresh schedule
   class StructureFire
-    def initialize(json)
-      @json = json
-
+    def initialize(jsons)
+      @jsons = jsons
     end
 
     def self.perform
-      new(json).perform
+      new(jsons).perform
     end
 
     def perform
-       fire_link =  ::StructureFireLink.find_or_initialize_by()
-       fire_link.assign_attributes(pdf: json[:pdf])
+      fire_link = ::StructureFireLink.find_or_initialize_by(url: jsons[0][:url], pdf_date_on: jsons[0][:pdf_date_on])
+      fire_link.pdf.attach(jsons[0][:pdf])
+      fire_link.save!
 
+      fire_structure = ::StructureFire.find_or_initialize_by(incident_number: jsons[1][:incident_number])
+      fire_structure.assign_attributes(jsons[1], structure_fire_link: fire_link.id)
+      fire_structure.save!
     end
   end
 end
