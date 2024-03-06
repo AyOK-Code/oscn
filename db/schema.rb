3d577e6c8f798e44ee55d093b24a6d3ad7a49ea9
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-03-06 15:20:02 -0600

    Add test for evictions mailerr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_21_175644) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_204347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
@@ -351,6 +351,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_21_175644) do
     t.datetime "sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "generated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
   create_table "eviction_letters", force: :cascade do |t|
--- spec/mailers/evictions_mailer_spec.rb
+++ spec/mailers/evictions_mailer_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe EvictionsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'file_email' do
+    let(:eviction_file) { create(:eviction_file, :with_file) }
+    let(:eviction_file_no_file) { create(:eviction_file) }
+
+    context 'when the file exists' do
+      it 'attaches the file' do
+        mail = described_class.file_email(eviction_file.id)
+
+        expect(mail.subject).to eq("Evictions File ID: #{eviction_file.id}")
+        expect(mail.attachments[eviction_file.file.filename.to_s]).to be_present
+      end
+
+      it 'sends the email' do
+        mail = described_class.file_email(eviction_file.id)
+
+        expect { mail.deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      end
+    end
+
+    context 'when the file does not exist' do
+      it 'does not send an email' do
+        mail = described_class.file_email(eviction_file_no_file.id)
+
+        expect { mail.deliver_now }.to raise_error('No file attached to the EvictionFile')
+      end
+    end
+  end
 end
