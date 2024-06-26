37d77fb4d0263217cd8eee8fe4be0012dc61bf59
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-06-26 11:39:55 -0500

    fix address

--- app/models/ok_sos/entity.rb
+++ app/models/ok_sos/entity.rb
@@ -1,4 +1,4 @@
 class OkSos::Entity < ApplicationRecord
   belongs_to :corp_type, class_name: 'OkSos::CorpType'
-  belongs_to :address, class_name: 'OkSos::Address', optional: true
+  belongs_to :entity_address, class_name: 'OkSos::EntityAddress', optional: true
 end
