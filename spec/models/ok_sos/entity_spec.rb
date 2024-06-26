db562c219cfcf63082b6f1534d426de0d6fe1307
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-06-26 11:40:11 -0500

    confirm entity fix

--- spec/models/ok_sos/entity_spec.rb
+++ spec/models/ok_sos/entity_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe OkSos::Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:corp_type).class_name('OkSos::CorpType') }
+  it { should belong_to(:entity_address).class_name('OkSos::EntityAddress').optional }
 end
