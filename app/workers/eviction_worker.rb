d4db3ab4eadb49851b79504416097912a05a11fa
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-07-12 11:21:57 -0500

    Queue eviction workers as high

--- app/workers/eviction_worker.rb
+++ app/workers/eviction_worker.rb
@@ -2,7 +2,7 @@
 class EvictionWorker
   include Sidekiq::Worker
   include Sidekiq::Throttled::Worker
-  sidekiq_options retry: 5
+  sidekiq_options retry: 5, queue: :high
   sidekiq_throttle_as :eviction_ocr
 
   def perform(letter_id)
