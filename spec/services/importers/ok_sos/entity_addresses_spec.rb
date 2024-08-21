996c48ddb25a15fd27729c60d6228375e6d5548c
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-08-21 14:43:44 -0500

    fix test

--- spec/services/importers/ok_sos/entity_addresses_spec.rb
+++ spec/services/importers/ok_sos/entity_addresses_spec.rb
@@ -4,6 +4,7 @@ require 'services/importers/ok_sos/shared_specs'
 RSpec.describe Importers::OkSos::EntityAddresses do
   it_behaves_like 'ok_sos_importer' do
     let(:sample_file) { 'spec/fixtures/importers/ok_sos/entity_addresses.csv' }
+    let!(:zip_code) { create(:zip_code, name: 73_103) }
     let(:record) { OkSos::EntityAddress.find_by!(address_id: 10) }
     let(:expected_attributes) do
       {
@@ -15,7 +16,7 @@ RSpec.describe Importers::OkSos::EntityAddresses do
         zip_string: '73103',
         zip_extension: nil,
         country: 'USA',
-        zip_code_id: ZipCode.find_by(name: 73_103).id
+        zip_code_id: zip_code.id
       }
     end
   end
