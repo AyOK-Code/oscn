730e2ab005e2d7cde70f03c9bb4eeca69d0bb22a
Sabrina Leggett <sabrina.gelbart@gmail.com>
2024-08-29 09:10:49 -0500

    Update zip_codes_spec.rb

--- spec/services/importers/ok_sos/zip_codes_spec.rb
+++ spec/services/importers/ok_sos/zip_codes_spec.rb
@@ -4,7 +4,7 @@ require 'services/importers/ok_sos/shared_specs'
 RSpec.describe Importers::OkSos::ZipCodes do
   it_behaves_like 'ok_sos_importer' do
     let(:sample_file) { 'spec/fixtures/importers/ok_sos/entity_addresses.csv' }
-    let(:record) { ZipCode.find_by(name: '73103') } # TODO: update this with real key?
+    let(:record) { ZipCode.find_by(name: '73103') }
     let(:expected_attributes) do
       {
         name: '73103'
