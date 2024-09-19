338c460cf405f96326603d8e633bafb69209d3e0
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-09-19 10:13:04 -0500

    make holden

--- app/mailers/case_html_error_mailer.rb
+++ app/mailers/case_html_error_mailer.rb
@@ -3,8 +3,8 @@ class CaseHtmlErrorMailer < ApplicationMailer
     @errors = errors
 
     mail(
-      to: ENV.fetch('INVALID_CASE_HTML_EMAIL_TO', 'sabrina.leggett@9bcorp.com'),
-      cc: ENV.fetch('INVALID_CASE_HTML_EMAIL_CC', 'sabrina.leggett@9bcorp.com').split(','),
+      to: ENV.fetch('INVALID_CASE_HTML_EMAIL_TO', 'hmitchell@9bcorp.com'),
+      cc: ENV.fetch('INVALID_CASE_HTML_EMAIL_CC', 'hmitchell@9bcorp.com').split(','),
       subject: "ERROR PARSING CASE HTML: sections #{errors.map{|x| x[:section]}.join(", ")}",
       reply_to: 'info@arnallfamilyfoundation.org'
     )
