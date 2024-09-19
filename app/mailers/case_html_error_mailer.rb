0daf80549a0a3ba641131de1aad897e98c304fee
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-09-19 10:24:03 -0500

    rubo

--- app/mailers/case_html_error_mailer.rb
+++ app/mailers/case_html_error_mailer.rb
@@ -5,7 +5,7 @@ class CaseHtmlErrorMailer < ApplicationMailer
     mail(
       to: ENV.fetch('INVALID_CASE_HTML_EMAIL_TO', 'hmitchell@9bcorp.com'),
       cc: ENV.fetch('INVALID_CASE_HTML_EMAIL_CC', 'hmitchell@9bcorp.com').split(','),
-      subject: "ERROR PARSING CASE HTML: sections #{errors.map{|x| x[:section]}.join(", ")}",
+      subject: "ERROR PARSING CASE HTML: sections #{errors.map { |x| x[:section] }.join(', ')}",
       reply_to: 'info@arnallfamilyfoundation.org'
     )
   end
--- app/services/case_html_validator/validate.rb
+++ app/services/case_html_validator/validate.rb
@@ -1,5 +1,4 @@
 module CaseHtmlValidator
-
   class CaseHtmlValidationError < StandardError
   end
 
@@ -13,37 +12,35 @@ module CaseHtmlValidator
       @errors = []
       @from_fixture = from_fixture
       @parsed_data = false
+      super()
     end
 
     def retrieve_html
-      begin
-        OscnScraper::Requestor::Case
-          .new({ county: CASE_COUNTY, number: CASE_NUMBER })
-          .fetch_case_by_number
-      rescue StandardError => e
-        @errors << {
-          section: "all",
-          message: "Unable to fetch #{CASE_NUMBER} from oscn.net. Errror: #{e}",
-        }
-        raise CaseHtmlValidationError
-      end
+      OscnScraper::Requestor::Case
+        .new({ county: CASE_COUNTY, number: CASE_NUMBER })
+        .fetch_case_by_number
+    rescue StandardError => e
+      @errors << {
+        section: 'all',
+        message: "Unable to fetch #{CASE_NUMBER} from oscn.net. Errror: #{e}"
+      }
+      raise CaseHtmlValidationError
     end
 
     def parse_html(case_html)
-      begin
-        parsed_html = Nokogiri::HTML(case_html)
-        OscnScraper::Parsers::BaseParser.new(parsed_html).build_object
-      rescue StandardError => e
-        @errors << {
-          section: "all",
-          message: "Unable to parse html for #{CASE_NUMBER}. Error: #{e}",
-        }
-        raise CaseHtmlValidationError
-      end
+      parsed_html = Nokogiri::HTML(case_html)
+      OscnScraper::Parsers::BaseParser.new(parsed_html).build_object
+    rescue StandardError => e
+      @errors << {
+        section: 'all',
+        message: "Unable to parse html for #{CASE_NUMBER}. Error: #{e}"
+      }
+      raise CaseHtmlValidationError
     end
 
     def parsed_data
       return @parsed_data if @parsed_data
+
       if from_fixture
         path = 'app/services/case_html_validator/expected/current_case_fixture.html'
         case_html = File.read(path)
@@ -57,8 +54,8 @@ module CaseHtmlValidator
     def perform
       begin
         validate
-      rescue CaseHtmlValidationError => e
-        test = e
+      rescue CaseHtmlValidationError
+        return handle_error
       end
       if @errors.length.zero?
         puts 'Case HTML parsed successfully'
@@ -78,7 +75,7 @@ module CaseHtmlValidator
     end
 
     def validate_section(section)
-      file_path = "app/services/case_html_validator/expected/#{section.to_s}.json"
+      file_path = "app/services/case_html_validator/expected/#{section}.json"
       begin
         expected = JSON.parse(File.read(file_path))
       rescue NoMethodError
@@ -86,13 +83,13 @@ module CaseHtmlValidator
       end
       actual = JSON.parse(parsed_data[section].to_json)
       differences = Hashdiff.diff(expected, actual, indifferent: true)
-      if differences.present?
-        @errors << {
-          section: section,
-          message: "Parsed data for #{:section} did not match",
-          differences: differences
-        }
-      end
+      return unless differences.present?
+
+      @errors << {
+        section: section,
+        message: 'Parsed data for section did not match',
+        differences: differences
+      }
     end
 
     def handle_error
