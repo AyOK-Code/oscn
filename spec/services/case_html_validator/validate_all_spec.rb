40ce474c22c0d7ea9599b21a34fba7ede4c9df5f
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-10-04 14:11:07 -0500

    fix html

--- spec/services/case_html_validator/validate_all_spec.rb
+++ spec/services/case_html_validator/validate_all_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe CaseHtmlValidator::ValidateAll do
   it 'successfully validates html' do
-    described_class.perform(from_fixture: true)
+    expectation = expect { described_class.perform(from_fixture: true) }
+    expectation.not_to output(/did not pass/).to_stdout
   end
 end
