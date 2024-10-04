a6ba888950b32e9dbca04f6890e1c65b8e83be47
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-10-04 11:51:18 -0500

    rubo

--- app/services/case_html_validator/validate_all.rb
+++ app/services/case_html_validator/validate_all.rb
@@ -1,12 +1,12 @@
 module CaseHtmlValidator
   class ValidateAll < ApplicationService
-    def perform()
+    def perform
       path = 'app/services/case_html_validator/expected/'
       Dir.foreach(path) do |county_directory|
-        next if county_directory == '.' or county_directory == '..'
+        next if (county_directory == '.') || (county_directory == '..')
 
-        Dir.foreach(path + '/' + county_directory) do |case_directory|
-          next if case_directory == '.' or case_directory == '..'
+        Dir.foreach("#{path}/#{county_directory}") do |case_directory|
+          next if (case_directory == '.') || (case_directory == '..')
 
           Validate.perform(county_directory, case_directory, from_fixture: false)
         end
