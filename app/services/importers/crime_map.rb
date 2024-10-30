bd03c564ba5741b3941940da4aec210bddf165f2
sabrinaleggett <sabrina.gelbart@gmail.com>
2024-10-30 13:33:08 -0500

    crime map scraper with missing analysis code

--- app/services/importers/crime_map.rb
+++ app/services/importers/crime_map.rb
@@ -4,112 +4,37 @@ require 'json'
 module Importers
   class CrimeMap < ApplicationService
     def initialize
-      @west = -97.674058
-      @south = 35.37686
-      @east = -97.140761
-      @north = 35.72598
+      zoom_okc_metro
+    end
 
+    def zoom_okc_metro
+      @east = -95.86508397102726
+      @north = 36.42498633282242
+      @south = 34.142898155956104
+      @west = -99.00717381477726
+      @lat = 35.283942244389266
+      @lng = -97.43612889290226
       @zoom = 9
-
-      @center_lat = 35.54464873256638
-      @center_lng = -97.49826954680444
     end
 
     def perform
-      # pins
-      require 'open-uri'
-      url = 'https://nacok.org/wp-content/uploads/2024/10/NA-Crime-Report-9-1-2024-to-9-30-2024.pdf'
-
-      io = URI.open(url)
-      reader = PDF::Reader.new(io)
-      puts reader.info
-      all_pages = []
-      reader.pages.each do |page|
-        all_pages << page_to_dict(page)
-      end
-      all_pages
+      get_crimes
     end
 
-    def page_to_dict(page)
-      header_index = 4
-
-      lines = page.text.split("\n\n")
-      header_row = lines[header_index]
-      table_rows = lines[(header_index + 1)...]
-                     .reject { |row| excluded_row?(row) }
-                     .compact_blank
-
-      return if table_rows.empty?
-
-      cols = columns(header_row)
-      page_dicts = []
-      table_rows.each do |row|
-        page_dicts << row_to_dict(cols, row)
-      end
-      page_dicts
-    end
-
-    def excluded_row?(row)
-      exclude_rows_with_text = [
-        'Total incidents',
-        'Reporting Dates:'
-      ]
-      exclude_rows_with_text.any? { |text| row.include?(text) }
-    end
-
-    def columns(header_row)
-      cols = {}
-      # Time is not included in column names so that it will be included with Date due to parsing difficulties
-      column_names = ['Address', 'Date', 'Offense', 'Description', 'Division', 'Case Number']
-      column_names.each_with_index do |column, i|
-        next_column = column_names[i + 1]
-        cols[column] = {
-          start: header_row.split(column)[0].length,
-          end: next_column ? header_row.split(next_column)[0].length : header_row.length+1
-        }
-      end
-      cols
-      # { 'Address': { start: 0, end: 28 },
-      #   'Date': { start: 28, end: 39 },
-      #   'Time': { start: 39, end: 49 },
-      #   'Offense': { start: 49, end: 104 },
-      #   'Description': { start: 104, end: 125 },
-      #   'Division': { start: 125, end: 140 },
-      #   'Case Number': { start: 140, end: 152 } }
-    end
-
-    def row_to_dict(cols, row)
-      row_dict = {}
-      cols.each do |column, indexes|
-        row_dict[column] = (row[indexes[:start]...indexes[:end]]).strip
-      end
-      row_dict
-    end
-
-    def pins
+    def get_crimes
       url = 'https://communitycrimemap.com/api/v1/search/load-data'
-      headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9,es;q=0.8,he;q=0.7',
-        'authorization': "Bearer #{token}",
-        'cache-control': 'no-cache',
-        'content-type': 'application/json',
-        'cookie': "ai_user=zPXLlny6Trh2li4D8X3aMv|2024-10-15T21:50:37.649Z; Authorization=Bearer%#{token}; ai_session=dgZ+KN34zuWTPY/g6EAPNV|1729693055912|1729693055912",
-        'origin': 'https://communitycrimemap.com',
-        'pragma': 'no-cache',
-        'priority': 'u=1, i',
-        'referer': 'https://communitycrimemap.com/map',
-        'request-id': '|baeb098886174d57ab8a31f3268d4ca3.f4da0b07c76247dc',
-        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'traceparent': '00-baeb098886174d57ab8a31f3268d4ca3-f4da0b07c76247dc-01',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
-      }
-      body = {
+      res = HTTParty.post(url, headers: pins_headers, body: pins_body)
+      crimes = JSON.parse(res.body)['data']['data']['pins'].map{|k,v| v['EventRecord']['MOs']['MO']}
+      crimes
+    end
+
+    def analyis(crimes)
+      min_date = crimes.sort_by {|v| v['DateTime']}[0]
+      max_date = crimes.sort_by {|v| v['DateTime']}[-1]
+    end
+
+    def pins_body
+      {
         'buffer' => {
           'enabled' => false,
           'restrictArea' => false,
@@ -141,9 +66,30 @@ module Importers
           }
         }
       }.to_json
-      res = HTTParty.post(url, headers: headers, body: body)
-      pins = JSON.parse(res.body)['data']['data']['pins']
-      pins
+    end
+
+    def pins_headers
+      {
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'en-US,en;q=0.9,es;q=0.8,he;q=0.7',
+        'authorization': "Bearer #{token}",
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'cookie': "ai_user=zPXLlny6Trh2li4D8X3aMv|2024-10-15T21:50:37.649Z; Authorization=Bearer%#{token}; ai_session=dgZ+KN34zuWTPY/g6EAPNV|1729693055912|1729693055912",
+        'origin': 'https://communitycrimemap.com',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': 'https://communitycrimemap.com/map',
+        'request-id': '|baeb098886174d57ab8a31f3268d4ca3.f4da0b07c76247dc',
+        'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'traceparent': '00-baeb098886174d57ab8a31f3268d4ca3-f4da0b07c76247dc-01',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+      }
     end
 
     def token
