5ddaee7dfb7bf9d61e50553629a79431e68bec23
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-11-13 11:55:18 -0600

    Drop and recreate dependent view

--- db/migrate/20241113174628_update_report_warrants_to_version_5.rb
+++ db/migrate/20241113174628_update_report_warrants_to_version_5.rb
@@ -1,8 +1,14 @@
 class UpdateReportWarrantsToVersion5 < ActiveRecord::Migration[7.0]
   def change
+    drop_view :report_searchable_cases, materialized: true
+
     update_view :report_warrants,
       version: 5,
       revert_to_version: 4,
+      materialized: true
+
+    create_view :report_searchable_cases,
+      version: 3,
       materialized: true
   end
 end
