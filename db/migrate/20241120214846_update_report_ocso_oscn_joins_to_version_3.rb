d08e9634522ee43d233a75dc81961f1817030dd4
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-11-20 16:06:08 -0600

    Add ocso joins column

---
+++ db/migrate/20241120214846_update_report_ocso_oscn_joins_to_version_3.rb
@@ -0,0 +1,8 @@
+class UpdateReportOcsoOscnJoinsToVersion3 < ActiveRecord::Migration[7.0]
+  def change
+    update_view :report_ocso_oscn_joins,
+      version: 3,
+      revert_to_version: 2,
+      materialized: true
+  end
+end
