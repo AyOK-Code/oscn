3aba7616a3ae9baa04784ed0e48a6cc9d4ed837e
sabrinaleggett <sabrina.gelbart@gmail.com>
2025-01-29 17:14:06 -0600

    allow county to be nullable

--- app/models/ok2_explore/death.rb
+++ app/models/ok2_explore/death.rb
@@ -1,5 +1,5 @@
 class Ok2Explore::Death < ApplicationRecord
-  belongs_to :county
+  belongs_to :county, optional: true
 
   validates :first_name, :last_name, :death_on, presence: true
   enum sex: [:male, :female]
